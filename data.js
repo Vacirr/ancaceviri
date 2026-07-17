59fa6e33fc0689d0f50dd318e1355f0c090048e4
Vacir <44121624+Vacirr@users.noreply.github.com>
2026-07-17 12:20:58 +0300

    Admin: data.js

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-// AncaÇeviri — 2026-03-09 10:07
+// AncaÇeviri — 2026-07-17 12:20
 const DATA = {
   "siteTitle": "ANCAÇEVİRİ",
   "siteDesc": "Çeviri değil, yerelleştirme yapıyoruz.",
@@ -16,7 +16,8 @@ const DATA = {
     "Site Yönetimi",
     "Genel Koordinasyon",
     "Teşekkürler",
-    "Proje Lideri"
+    "Proje Lideri",
+    "Retried"
   ],
   "games": [
     {
@@ -26,7 +27,7 @@ const DATA = {
       "details": "Xenogears, JRPG türünün zirve noktalarından biri olarak kabul edilen, felsefe ve psikolojiyi bilimkurguyla harmanlayan kült bir yapımdır. Hikâye, hafızasını kaybetmiş genç bir ressam olan Fei Fong Wong’un, devasa bir yıkımın ardından kendi geçmişini ve insanlığın kökenlerini sorguladığı epik bir yolculuğu konu alır.",
       "translationDetails": "Yerelleştirme çalışması 'Perfect World Build' referans alınarak yapılmıştır.",
       "completion": 0,
-      "status": "Devam Ediyor",
+      "status": "Durduruldu",
       "lastUpdate": "2026-03-09",
       "downloadLink": "",
       "downloads": 0,
@@ -79,7 +80,7 @@ const DATA = {
       "image": "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/523000/capsule_616x353.jpg?t=1728354404",
       "details": "Princess Maker 2 Refine, klasik yaşam simülasyonu türünün en köklü ve sevilen örneklerinden biri olan orijinal yapımın, modernize edilmiş grafiklerle yeniden karşımıza çıkan halidir. Gökyüzünden sana emanet edilen bir kızı, 10 yaşından 18 yaşına kadar eğitmek, büyütmek ve geleceğini şekillendirmek tamamen senin ellerinde.",
       "translationDetails": "",
-      "completion": 0,
+      "completion": 45,
       "status": "Devam Ediyor",
       "lastUpdate": "2026-03-09",
       "downloadLink": "",
@@ -97,33 +98,66 @@ const DATA = {
           "gameRole": "Proje Lideri"
         }
       ]
+    },
+    {
+      "id": "jade-empire-special-edition-mroq5xt0",
+      "title": "Jade Empire™: Special Edition",
+      "image": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/7110/header.jpg?t=1777494940",
+      "details": "BioWare tarafından geliştirilen ve Uzak Doğu mitolojisinden esinlenen, aksiyon odaklı bir rol yapma oyunudur.",
+      "translationDetails": "",
+      "completion": 0,
+      "status": "Devam Ediyor",
+      "lastUpdate": "2026-07-17",
+      "downloadLink": "",
+      "downloads": 0,
+      "installGuide": "",
+      "screenshots": [],
+      "contributors": [
+        {
+          "memberId": "mete-karace-vacir-mmirvumd",
+          "gameRoles": [
+            "Programcı",
+            "Genel Koordinasyon"
+          ],
+          "gameRole": "Programcı"
+        },
+        {
+          "memberId": "sezar-mroq78mf",
+          "gameRoles": [
+            "Proje Lideri",
+            "Kalite Kontrol",
+            "Editör",
+            "Çevirmen"
+          ],
+          "gameRole": "Proje Lideri"
+        }
+      ]
     }
   ],
   "team": [
     {
       "id": "mete-karace-vacir-mmirvumd",
       "name": "Mete Karace ~ Vacir",
-      "photo": "",
-      "role": "Genel",
       "roles": [
         "Genel",
         "Çevirmen",
-        "Site Yönetimi"
+        "Site Yönetimi",
+        "Genel Koordinasyon"
       ],
-      "tasks": ""
+      "role": "Genel",
+      "tasks": "",
+      "photo": ""
     },
     {
       "id": "xoluos-mmirxen9",
       "name": "Xoluos",
-      "photo": "",
-      "role": "Çevirmen",
       "roles": [
         "Çevirmen",
-        "Editör",
-        "Genel",
-        "Genel Koordinasyon"
+        "Editör"
       ],
-      "tasks": ""
+      "role": "Çevirmen",
+      "tasks": "",
+      "photo": ""
     },
     {
       "id": "bendisdsgn-mmis2kkq",
@@ -140,22 +174,34 @@ const DATA = {
     {
       "id": "order6-mmis30ms",
       "name": "order6",
-      "photo": "",
-      "role": "Çevirmen",
       "roles": [
-        "Çevirmen"
+        "Çevirmen",
+        "Retried"
       ],
-      "tasks": ""
+      "role": "Çevirmen",
+      "tasks": "",
+      "photo": ""
     },
     {
       "id": "anil-mmis3cle",
       "name": "Anıl",
-      "photo": "",
-      "role": "Kalite Kontrol",
       "roles": [
-        "Kalite Kontrol"
+        "Kalite Kontrol",
+        "Retried"
       ],
-      "tasks": ""
+      "role": "Kalite Kontrol",
+      "tasks": "",
+      "photo": ""
+    },
+    {
+      "id": "sezar-mroq78mf",
+      "name": "Sezar",
+      "roles": [
+        "Çevirmen"
+      ],
+      "role": "Çevirmen",
+      "tasks": "",
+      "photo": ""
     }
   ]
 };
