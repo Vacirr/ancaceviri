18719628935a40e7fdefe0cc084f247eaee44629
Vacir <44121624+Vacirr@users.noreply.github.com>
2026-07-18 18:07:32 +0300

    Admin: data.js

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-// AncaÇeviri — 2026-07-17 12:20
+// AncaÇeviri — 2026-07-18 18:07
 const DATA = {
   "siteTitle": "ANCAÇEVİRİ",
   "siteDesc": "Çeviri değil, yerelleştirme yapıyoruz.",
@@ -21,14 +21,161 @@ const DATA = {
   ],
   "games": [
     {
-      "id": "xenogears-mmiry3c9",
-      "title": "Xenogears",
-      "image": "https://cdn.mobygames.com/covers/2583963-xenogears-playstation-front-cover.jpg",
-      "details": "Xenogears, JRPG türünün zirve noktalarından biri olarak kabul edilen, felsefe ve psikolojiyi bilimkurguyla harmanlayan kült bir yapımdır. Hikâye, hafızasını kaybetmiş genç bir ressam olan Fei Fong Wong’un, devasa bir yıkımın ardından kendi geçmişini ve insanlığın kökenlerini sorguladığı epik bir yolculuğu konu alır.",
-      "translationDetails": "Yerelleştirme çalışması 'Perfect World Build' referans alınarak yapılmıştır.",
       "completion": 0,
-      "status": "Durduruldu",
+      "contributors": [
+        {
+          "gameRole": "Proje Lideri",
+          "gameRoles": [
+            "Proje Lideri",
+            "Çevirmen",
+            "Editör"
+          ],
+          "memberId": "xoluos-mmirxen9"
+        },
+        {
+          "gameRole": "Teşekkürler",
+          "gameRoles": [
+            "Teşekkürler"
+          ],
+          "memberId": "mete-karace-vacir-mmirvumd"
+        },
+        {
+          "gameRole": "Çevirmen",
+          "gameRoles": [
+            "Çevirmen",
+            "Editör"
+          ],
+          "memberId": "bendisdsgn-mmis2kkq"
+        },
+        {
+          "gameRole": "Çevirmen",
+          "gameRoles": [
+            "Çevirmen"
+          ],
+          "memberId": "order6-mmis30ms"
+        },
+        {
+          "gameRole": "Kalite Kontrol",
+          "gameRoles": [
+            "Kalite Kontrol"
+          ],
+          "memberId": "anil-mmis3cle"
+        }
+      ],
+      "details": "Xenogears, JRPG türünün zirve noktalarından biri olarak kabul edilen, felsefe ve psikolojiyi bilimkurguyla harmanlayan kült bir yapımdır. Hikâye, hafızasını kaybetmiş genç bir ressam olan Fei Fong Wong’un, devasa bir yıkımın ardından kendi geçmişini ve insanlığın kökenlerini sorguladığı epik bir yolculuğu konu alır.",
+      "downloadLink": "",
+      "downloads": 0,
+      "id": "xenogears-mmiry3c9",
+      "image": "https://cdn.mobygames.com/covers/2583963-xenogears-playstation-front-cover.jpg",
+      "installGuide": "",
       "lastUpdate": "2026-03-09",
+      "screenshots": [],
+      "status": "Durduruldu",
+      "title": "Xenogears",
+      "translationDetails": "Yerelleştirme çalışması 'Perfect World Build' referans alınarak yapılmıştır."
+    },
+    {
+      "completion": 45,
+      "contributors": [
+        {
+          "gameRole": "Proje Lideri",
+          "gameRoles": [
+            "Proje Lideri",
+            "Çevirmen",
+            "Editör"
+          ],
+          "memberId": "mete-karace-vacir-mmirvumd"
+        }
+      ],
+      "details": "Princess Maker 2 Refine, klasik yaşam simülasyonu türünün en köklü ve sevilen örneklerinden biri olan orijinal yapımın, modernize edilmiş grafiklerle yeniden karşımıza çıkan halidir. Gökyüzünden sana emanet edilen bir kızı, 10 yaşından 18 yaşına kadar eğitmek, büyütmek ve geleceğini şekillendirmek tamamen senin ellerinde.",
+      "downloadLink": "",
+      "downloads": 0,
+      "id": "princess-maker-2-refine-mmiu9i60",
+      "image": "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/523000/capsule_616x353.jpg?t=1728354404",
+      "installGuide": "",
+      "lastUpdate": "2026-03-09",
+      "screenshots": [],
+      "status": "Devam Ediyor",
+      "title": "Princess Maker 2 Refine",
+      "translationDetails": ""
+    },
+    {
+      "completion": 0,
+      "contributors": [
+        {
+          "gameRole": "Programcı",
+          "gameRoles": [
+            "Programcı",
+            "Genel Koordinasyon"
+          ],
+          "memberId": "mete-karace-vacir-mmirvumd"
+        },
+        {
+          "gameRole": "Proje Lideri",
+          "gameRoles": [
+            "Proje Lideri",
+            "Kalite Kontrol",
+            "Editör",
+            "Çevirmen"
+          ],
+          "memberId": "sezar-mroq78mf"
+        }
+      ],
+      "details": "BioWare tarafından geliştirilen ve Uzak Doğu mitolojisinden esinlenen, aksiyon odaklı bir rol yapma oyunudur.",
+      "downloadLink": "",
+      "downloads": 0,
+      "id": "jade-empire-special-edition-mroq5xt0",
+      "image": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/7110/header.jpg?t=1777494940",
+      "installGuide": "",
+      "lastUpdate": "2026-07-17",
+      "screenshots": [],
+      "status": "Devam Ediyor",
+      "title": "Jade Empire™: Special Edition",
+      "translationDetails": ""
+    },
+    {
+      "id": "ninja-gaiden-nes-mrqhzroi",
+      "title": "Ninja Gaiden (NES)",
+      "image": "https://i.pinimg.com/736x/66/67/5c/66675c17f7fbe3f7b6bdae6197635ef4.jpg",
+      "details": "Ninja Gaiden",
+      "translationDetails": "",
+      "completion": 0,
+      "status": "Devam Ediyor",
+      "lastUpdate": "2026-07-18",
+      "downloadLink": "",
+      "downloads": 0,
+      "installGuide": "",
+      "screenshots": [],
+      "contributors": [
+        {
+          "memberId": "bendisdsgn-mmis2kkq",
+          "gameRoles": [
+            "Çevirmen",
+            "Editör",
+            "Kalite Kontrol",
+            "Proje Lideri",
+            "Genel Koordinasyon"
+          ],
+          "gameRole": "Çevirmen"
+        },
+        {
+          "memberId": "xoluos-mmirxen9",
+          "gameRoles": [
+            "Teşekkürler"
+          ],
+          "gameRole": "Teşekkürler"
+        }
+      ]
+    },
+    {
+      "id": "kirby-s-adventure-nes-mrqi3cma",
+      "title": "Kirby's Adventure (NES)",
+      "image": "https://raw.githubusercontent.com/Vacirr/ancaceviri/master/uploads/img_20260718_180652_213455.webp",
+      "details": "Kirby's Adventure",
+      "translationDetails": "",
+      "completion": 0,
+      "status": "Devam Ediyor",
+      "lastUpdate": "2026-07-18",
       "downloadLink": "",
       "downloads": 0,
       "installGuide": "",
@@ -37,99 +184,11 @@ const DATA = {
         {
           "memberId": "xoluos-mmirxen9",
           "gameRoles": [
-            "Proje Lideri",
-            "Çevirmen",
-            "Editör"
-          ],
-          "gameRole": "Proje Lideri"
-        },
-        {
-          "memberId": "mete-karace-vacir-mmirvumd",
-          "gameRoles": [
-            "Teşekkürler"
-          ],
-          "gameRole": "Teşekkürler"
-        },
-        {
-          "memberId": "bendisdsgn-mmis2kkq",
-          "gameRoles": [
-            "Çevirmen",
-            "Editör"
-          ],
-          "gameRole": "Çevirmen"
-        },
-        {
-          "memberId": "order6-mmis30ms",
-          "gameRoles": [
-            "Çevirmen"
-          ],
-          "gameRole": "Çevirmen"
-        },
-        {
-          "memberId": "anil-mmis3cle",
-          "gameRoles": [
-            "Kalite Kontrol"
-          ],
-          "gameRole": "Kalite Kontrol"
-        }
-      ]
-    },
-    {
-      "id": "princess-maker-2-refine-mmiu9i60",
-      "title": "Princess Maker 2 Refine",
-      "image": "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/523000/capsule_616x353.jpg?t=1728354404",
-      "details": "Princess Maker 2 Refine, klasik yaşam simülasyonu türünün en köklü ve sevilen örneklerinden biri olan orijinal yapımın, modernize edilmiş grafiklerle yeniden karşımıza çıkan halidir. Gökyüzünden sana emanet edilen bir kızı, 10 yaşından 18 yaşına kadar eğitmek, büyütmek ve geleceğini şekillendirmek tamamen senin ellerinde.",
-      "translationDetails": "",
-      "completion": 45,
-      "status": "Devam Ediyor",
-      "lastUpdate": "2026-03-09",
-      "downloadLink": "",
-      "downloads": 0,
-      "installGuide": "",
-      "screenshots": [],
-      "contributors": [
-        {
-          "memberId": "mete-karace-vacir-mmirvumd",
-          "gameRoles": [
-            "Proje Lideri",
-            "Çevirmen",
-            "Editör"
-          ],
-          "gameRole": "Proje Lideri"
-        }
-      ]
-    },
-    {
-      "id": "jade-empire-special-edition-mroq5xt0",
-      "title": "Jade Empire™: Special Edition",
-      "image": "https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/7110/header.jpg?t=1777494940",
-      "details": "BioWare tarafından geliştirilen ve Uzak Doğu mitolojisinden esinlenen, aksiyon odaklı bir rol yapma oyunudur.",
-      "translationDetails": "",
-      "completion": 0,
-      "status": "Devam Ediyor",
-      "lastUpdate": "2026-07-17",
-      "downloadLink": "",
-      "downloads": 0,
-      "installGuide": "",
-      "screenshots": [],
-      "contributors": [
-        {
-          "memberId": "mete-karace-vacir-mmirvumd",
-          "gameRoles": [
-            "Programcı",
-            "Genel Koordinasyon"
-          ],
-          "gameRole": "Programcı"
-        },
-        {
-          "memberId": "sezar-mroq78mf",
-          "gameRoles": [
-            "Proje Lideri",
-            "Kalite Kontrol",
             "Editör",
-            "Çevirmen"
+            "Çevirmen",
+            "Proje Lideri"
           ],
-          "gameRole": "Proje Lideri"
+          "gameRole": "Editör"
         }
       ]
     }
@@ -138,26 +197,26 @@ const DATA = {
     {
       "id": "mete-karace-vacir-mmirvumd",
       "name": "Mete Karace ~ Vacir",
+      "photo": "",
+      "role": "Genel",
       "roles": [
         "Genel",
         "Çevirmen",
         "Site Yönetimi",
         "Genel Koordinasyon"
       ],
-      "role": "Genel",
-      "tasks": "",
-      "photo": ""
+      "tasks": ""
     },
     {
       "id": "xoluos-mmirxen9",
       "name": "Xoluos",
+      "photo": "",
+      "role": "Çevirmen",
       "roles": [
         "Çevirmen",
         "Editör"
       ],
-      "role": "Çevirmen",
-      "tasks": "",
-      "photo": ""
+      "tasks": ""
     },
     {
       "id": "bendisdsgn-mmis2kkq",
@@ -174,34 +233,34 @@ const DATA = {
     {
       "id": "order6-mmis30ms",
       "name": "order6",
+      "photo": "",
+      "role": "Çevirmen",
       "roles": [
         "Çevirmen",
         "Retried"
       ],
-      "role": "Çevirmen",
-      "tasks": "",
-      "photo": ""
+      "tasks": ""
     },
     {
       "id": "anil-mmis3cle",
       "name": "Anıl",
+      "photo": "",
+      "role": "Kalite Kontrol",
       "roles": [
         "Kalite Kontrol",
         "Retried"
       ],
-      "role": "Kalite Kontrol",
-      "tasks": "",
-      "photo": ""
+      "tasks": ""
     },
     {
       "id": "sezar-mroq78mf",
       "name": "Sezar",
+      "photo": "",
+      "role": "Çevirmen",
       "roles": [
         "Çevirmen"
       ],
-      "role": "Çevirmen",
-      "tasks": "",
-      "photo": ""
+      "tasks": ""
     }
   ]
 };
